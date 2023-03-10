3365b2a277ccb1970a2411bc3f4e717ac0dc00b5
Dominic-Krelling <dominic.krelling@gmail.com>
2023-03-10 16:34:55 -0300

    COMIT INICIAL

---
+++ gar/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Categoria(models.Model):
+    descricao = models.CharField(max_length=100)
+    def __str__(self):
+        return self.descricao
+
+class Marca(models.Model):
+    nome = models.CharField(max_length=100)
+    site = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return self.nome

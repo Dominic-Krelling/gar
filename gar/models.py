baa38c8e28594bac71811c9f3a5d277b067418e2
Dominic-Krelling <dominic.krelling@gmail.com>
2023-03-14 15:08:03 -0300

    Criação da nacionalidade

--- gar/models.py
+++ gar/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 
+
 class Categoria(models.Model):
     descricao = models.CharField(max_length=100)
+
     def __str__(self):
         return self.descricao
 
+
 class Marca(models.Model):
-    nome = models.CharField(max_length=100)
-    nacionalidade = models.CharField(max_length=50)
+    nome = models.CharField(max_length=50)
+    nacionalidade = models.CharField(max_length=50, null=True, blank=True)
+
     def __str__(self):
-        return self.nome
+        return self.nome.upper()

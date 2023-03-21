27c67b417d093b969aa5c92a9b4d6c91f908a1bf
Dominic-Krelling <dominic.krelling1@gmail.com>
2023-03-21 15:02:38 -0300

    criação do acessorio,cor e veiculo

--- gar/admin.py
+++ gar/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from gar.models import Categoria, Marca
+from gar.models import Categoria, Marca, Acessório, Cor, Veiculo
 
 admin.site.register(Categoria)
 admin.site.register(Marca)
+admin.site.register(Cor)
+admin.site.register(Acessório)
+admin.site.register(Veiculo)
--- gar/models.py
+++ gar/models.py
@@ -14,3 +14,31 @@ class Marca(models.Model):
 
     def __str__(self):
         return self.nome.upper()
+
+class Acessório(models.Model):
+    descrição = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descrição
+    
+class Cor(models.Model):
+    descrição = models.CharField(max_length=100)
+
+    def __str__ (self):
+        return self.descrição
+    
+class Veiculo(models.Model):
+    ano = models.IntegerField(default=0, null=True, blank=True)
+    preco = models.DecimalField(
+        max_digits=10,decimal_places=2,default=0, null=True, blank= True)
+    modelo = models.CharField(max_length=50)
+    marca = models.ForeignKey(
+        Marca, on_delete=models.PROTECT, related_name="veiculos")
+    categoria = models.ForeignKey(
+        Categoria,on_delete=models.PROTECT, related_name="veiculos")
+    cor = models.ForeignKey(
+        Cor,on_delete=models.PROTECT, related_name="veiculos")
+
+
+    def __str__ (self):
+        return f"{self.modelo} ({self.id})"
